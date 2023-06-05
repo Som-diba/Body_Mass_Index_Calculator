ee72b4057f7dd3a284205cc4f2eaac5c1f0a6e86
Som-diba <ifedibac.chisom@gmail.com>
2023-06-05 11:57:40 +0000

    BMI calculator updated

--- BMI-Calculator.py
+++ BMI-Calculator.py
@@ -1,33 +1,51 @@
-print("Adult BMI Calculator.")
+def calculate_bmi(weight, height):
+  #Conversion fators
+  lbs_to_kg = 0.4536
+  in_to_m = 0.0254
 
+  #Conversion of weight from pounds to kilogram
+  if unit_of_weight == "lbs":
+    weight = weight * lbs_to_kg
+
+  #Conversion height to meters
+  if unit_of_height == "cm":
+    height = height / 100  # Conversion of height from centimeters to meters
+  elif unit_of_height == "ft_in":
+    height = str(height)
+    #Spliting of height into feet and inches
+    feet, inches = height.split(".")
+    height = (float(feet) * 12 + float(inches)
+              ) * in_to_m  #Conversion of height from feet_inches to meters
+
+  #Calculating BMI using the formula
+  bmi = weight / (height**2)
+  round_bmi = round(bmi, 2)  #rounding number to 2 decimal places
+  return round_bmi
+
+
+def suggest_meals(bmi):
+  if bmi < 18.5:
+    suggestion = "You are underweight.\nYou may consider meals rich in protein and healthy fats. Examples include eggs, lean meats, fish, nuts, and avocados."
+  elif bmi >= 18.5 and bmi < 24.9:
+    suggestion = "Your weight is normal.\nMaintain a balanced diet with a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats."
+  elif bmi >= 24.9 and bmi < 29.9:
+    suggestion = "You are overweight.\nFocus on portion control and incorporate more fruits, vegetables, whole grains, lean proteins, and low-fat dairy into your meals."
+  else:
+    suggestion = "You are in the obese range.\nIt is advisable to consult a healthcare professional or nutritionist for a personalized meal plan."
+
+  return suggestion
+
+
+#assigning variables
 age = int(input("Enter your age: "))
-height = float(input("Enter your height in centimeters: "))
-weight = float(input("Enter your weight in kilogram: "))
+weight = float(input("Enter your weight: "))
+unit_of_weight = input("Enter unit of weight (kg, lbs): ")
+height = float(input("Enter your height: "))
+unit_of_height = input("Enter unit of height (ft_in (eg. 5.6), cm, m): ")
 
+bmi = calculate_bmi(weight, height)
+print("Body Mass Index (BMI):", bmi)
 
-def calculate_adultbmi(weight, height):
-  # Conversion from height in centimeters to meters
-  height_m = height / 100
+meal_suggestion = suggest_meals(bmi)
 
-  # Calculating BMI using the formula
-  bmi = weight / (height_m**2)
-  return bmi
-
-  print("BMI:", bmi)
-
-  if age >= 20:
-
-    def suggest_meals(bmi):
-      if bmi < 18.5:
-        suggestion = "You are underweight. You may consider meals rich in protein and healthy fats. Examples include eggs, lean meats, fish, nuts, and avocados."
-      elif bmi >= 18.5 and bmi < 24.9:
-        suggestion = "Your weight is normal. Maintain a balanced diet with a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats."
-      elif bmi >= 24.9 and bmi < 29.9:
-        suggestion = "You are overweight. Focus on portion control and incorporate more fruits, vegetables, whole grains, lean proteins, and low-fat dairy into your meals."
-      else:
-        suggestion = "You are in the obese range. It is advisable to consult a healthcare professional or nutritionist for a personalized meal plan."
-
-      return suggestion
-
-    meal_suggestion = suggest_meals(bmi)
-    print("Meal suggestion:", meal_suggestion)
+print("Meal suggestion:", meal_suggestion)
