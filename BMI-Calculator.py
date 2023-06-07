22fb0b8a73fd24ce154af91ef1773935ec75cfca
Som-diba <ifedibac.chisom@gmail.com>
2023-06-07 04:06:03 +0000

    README updated

--- BMI-Calculator.py
+++ BMI-Calculator.py
@@ -5,17 +5,22 @@ def calculate_bmi(weight, height):
 
   #Conversion of weight from pounds to kilogram
   if unit_of_weight == "lbs":
-    weight = weight * lbs_to_kg
+    weight = float(weight) * lbs_to_kg
+  else:
+    weight = float(weight)
 
   #Conversion height to meters
   if unit_of_height == "cm":
-    height = height / 100  # Conversion of height from centimeters to meters
+    height = float(
+      height) / 100  # Conversion of height from centimeters to meters
+
   elif unit_of_height == "ft_in":
-    height = str(height)
     #Spliting of height into feet and inches
-    feet, inches = height.split(".")
+    feet, inches = height.split("'")
     height = (float(feet) * 12 + float(inches)
               ) * in_to_m  #Conversion of height from feet_inches to meters
+  elif unit_of_height == "m":
+    height = float(height)
 
   #Calculating BMI using the formula
   bmi = weight / (height**2)
@@ -25,27 +30,26 @@ def calculate_bmi(weight, height):
 
 def suggest_meals(bmi):
   if bmi < 18.5:
-    suggestion = "You are underweight.\nYou may consider meals rich in protein and healthy fats. Examples include eggs, lean meats, fish, nuts, and avocados."
-  elif bmi >= 18.5 and bmi < 24.9:
-    suggestion = "Your weight is normal.\nMaintain a balanced diet with a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats."
-  elif bmi >= 24.9 and bmi < 29.9:
-    suggestion = "You are overweight.\nFocus on portion control and incorporate more fruits, vegetables, whole grains, lean proteins, and low-fat dairy into your meals."
-  else:
-    suggestion = "You are in the obese range.\nIt is advisable to consult a healthcare professional or nutritionist for a personalized meal plan."
+    suggestion = "You are underweight.\nSuggestion: You may consider meals rich in protein and healthy fats. Examples include eggs, lean meats, fish, nuts, and avocados."
 
+  elif 18.5 <= bmi < 24.9:
+    suggestion = "Your weight is normal.\nSuggestion: Maintain a balanced diet with a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats."
+  elif 24.9 <= bmi < 29.9:
+    suggestion = "You are overweight.\nSuggestion: Focus on portion control and incorporate more fruits, vegetables, whole grains, lean proteins, and low-fat dairy into your meals."
+  else:
+    suggestion = "You are in the obese range.\nSuggestion: It is advisable to consult a healthcare professional or nutritionist for a personalized meal plan."
   return suggestion
 
 
 #assigning variables
-age = int(input("Enter your age: "))
-weight = float(input("Enter your weight: "))
+weight = input("Enter your weight: ")
 unit_of_weight = input("Enter unit of weight (kg, lbs): ")
-height = float(input("Enter your height: "))
-unit_of_height = input("Enter unit of height (ft_in (eg. 5.6), cm, m): ")
+height = input("Enter your height (eg. 5'6 for ft_in): ")
+unit_of_height = input("Enter unit of height (ft_in, cm, m): ")
 
 bmi = calculate_bmi(weight, height)
 print("Body Mass Index (BMI):", bmi)
 
 meal_suggestion = suggest_meals(bmi)
 
-print("Meal suggestion:", meal_suggestion)
+print("Result:", meal_suggestion)
--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-# BMI-Calculator
+# Adult BMI Calculator
 **Redi Project**
 
 This is a Body Mass Index (BMI) Calculator. The Body Mass Index of an individual is a numerial value or measure that uses the height and the weight to work out and access whether the individual has a healthy body weight relative to their height.
 
-The BMI does not take into account other factors such as muscle mass, body composition, age, health challenges and so on. It is always recommended to consult a healthcare professional for a comprehensive assessment of an individual's health.
+**Who can use the calculator**
 
-The output of this calculator returns if you underweight, normal weight, overwight or obese. Based on the output and  the age of the individual, it suggests food, vegetables and fruits that can be taken to either add weight, maintain weight or reduce weight.
+The BMI calculator is suitable for adults from 18 years old and over.
+
+The output of this calculator returns a result and tell if you are underweight, normal weight, overwight or obese.
+
+A BMI:
+1. below 18.5 is underweight
+2. equal to 18.5 or less than 24.9 is healthy
+3. equal to 25 or less than 29.9 is overweight
+4. of 30 or over is obese
+
+**How the BMI is calculated**
+
+The BMI calculation divides the weight in kilograms (kg) by their height in metres (m) squared.
+
+For example, if you weigh 70kg (around 154 pounds) and are 1.73m (around 5 feet 8 inches or 173cm) tall, you work out your BMI by:
+
+1. squaring your height: 1.73x1.73 = 2.99
+2. dividing 70 by 2.99 = 23.41137
+
++ If value for weight is entered in pounds, it converts to kg by a multiplying factor of 0.4536
++ If value for height is entered in centimeters, it converts to meters by dividing by 100
++  If value for height is entered in feets and inches, it converts to meters by first converting feets to inches by multiplying by 12, add the result to the value of inches then multiply the sum by a factor of 0.0254
++  Your result will be displayed to 2 decimal place, for example 23.41.
++  Based on the result and the range it falls in, it suggests food, vegetables and fruits that can be taken to either add weight, maintain weight or reduce weight.
+    
+**Limitations of the BMI**
+
+1. The BMI does not take into account other factors such as muscle mass, body composition, age, health challenges and so on. It is always recommended to consult a healthcare professional for a comprehensive assessment of an individual's health.
+
+2. The BMI calculator may not be suitable if you think you have an eating disorder or are being treated for one. Ask a health practitioner for advice.
+
+3. If you are pregnant, use your pre-pregnancy weight when calculating your BMI.
